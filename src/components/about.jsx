8f4019291980b4d058faf7416f5cefcd148cfda4
edgarrincon <edgarrinconmoranml@gmail.com>
2021-03-31 18:28:37 -0400

    npm fade test

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,3 +1,5 @@
+import Fade from "react-reveal/Fade";
+
 export const About = (props) => {
   return (
     <div id="about">
@@ -5,38 +7,42 @@ export const About = (props) => {
         <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img
-              src="img/about.jpg"
-              className="img-responsive"
-              alt="nosotros"
-            />{" "}
+            <Fade left duration={1000} delay={600} distance="30px">
+              <img
+                src="img/about.jpg"
+                className="img-responsive"
+                alt="nosotros"
+              />{" "}
+            </Fade>
           </div>
           <div className="col-xs-12 col-md-6">
-            <div className="about-text">
-              <h2>Nosotros</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>¿Por qué escogernos?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-6">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-6">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
+            <Fade right duration={1000} delay={600} distance="30px">
+              <div className="about-text">
+                <h2>Nosotros</h2>
+                <p>{props.data ? props.data.paragraph : "loading..."}</p>
+                <h3>¿Por qué escogernos?</h3>
+                <div className="list-style">
+                  <div className="col-lg-6 col-sm-6 col-xs-6">
+                    <ul>
+                      {props.data
+                        ? props.data.Why.map((d, i) => (
+                            <li key={`${d}-${i}`}>{d}</li>
+                          ))
+                        : "loading"}
+                    </ul>
+                  </div>
+                  <div className="col-lg-6 col-sm-6 col-xs-6">
+                    <ul>
+                      {props.data
+                        ? props.data.Why2.map((d, i) => (
+                            <li key={`${d}-${i}`}> {d}</li>
+                          ))
+                        : "loading"}
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Fade>
           </div>
         </div>
       </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,5 @@
+import Fade from "react-reveal/Fade";
+
 export const Header = (props) => {
   return (
     <header id="header">
@@ -6,14 +8,19 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a href="#about" className="btn btn-custom btn-lg page-scroll">
-                  Nosotros
-                </a>{" "}
+                <Fade bottom duration={1000} delay={600} distance="30px">
+                  <h1>
+                    {props.data ? props.data.title : "Loading"}
+                    <span></span>
+                  </h1>
+                  <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                  <a
+                    href="#about"
+                    className="btn btn-custom btn-lg page-scroll"
+                  >
+                    Nosotros
+                  </a>{" "}
+                </Fade>
               </div>
             </div>
           </div>
