34b2f08cd7b8a56026b86dd4a88e54ed41125a63
edgarrincon <edgarrinconmoranml@gmail.com>
2021-04-01 22:30:04 -0400

    orden gallery

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -19,6 +19,74 @@ export const Gallery = (props) => {
                 <div className="portfolio-item">
                   <div className="hover-bg">
                     {" "}
+                    <a href="https://venezuela.justia.com/federales/leyes-organicas/ley-organica-para-la-proteccion-del-nino-y-del-adolescente/gdoc/">
+                      <div className="hover-text">
+                        <img src="img/logoGPA.png" alt="logo"></img>
+                      </div>{" "}
+                      <img
+                        src="img/portfolio/06-small.jpg"
+                        className="img-responsive img-gallery"
+                        alt="Project Title"
+                      />
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a href="https://venezuela.justia.com/federales/codigos/codigo-organico-procesal-penal/gdoc/">
+                      <div className="hover-text">
+                        <img src="img/logoGPA.png" alt="logo"></img>
+                      </div>{" "}
+                      <img
+                        src="img/portfolio/copp.jpg"
+                        className="img-responsive img-gallery"
+                        alt="Project Title"
+                      />
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a href="https://venezuela.justia.com/federales/constitucion-de-la-republica-bolivariana-de-venezuela/">
+                      <div className="hover-text">
+                        <img src="img/logoGPA.png" alt="logo"></img>
+                      </div>{" "}
+                      <img
+                        src="img/portfolio/constitucion.jpg"
+                        className="img-responsive img-gallery"
+                        alt="Project Title"
+                      />
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
+                    <a href="https://venezuela.justia.com/federales/codigos/reforma-del-codigo-penal/gdoc/">
+                      <div className="hover-text">
+                        <img src="img/logoGPA.png" alt="logo"></img>
+                      </div>{" "}
+                      <img
+                        src="img/portfolio/codigopenal.jpg"
+                        className="img-responsive img-gallery"
+                        alt="Project Title"
+                      />
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item">
+                  <div className="hover-bg">
+                    {" "}
                     <a href="https://venezuela.justia.com/federales/constitucion-de-la-republica-bolivariana-de-venezuela/">
                       <div className="hover-text">
                         <img src="img/logoGPA.png" alt="logo"></img>
@@ -93,74 +161,6 @@ export const Gallery = (props) => {
                       </div>{" "}
                       <img
                         src="img/portfolio/05-small.jpg"
-                        className="img-responsive img-gallery"
-                        alt="Project Title"
-                      />
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a href="https://venezuela.justia.com/federales/leyes-organicas/ley-organica-para-la-proteccion-del-nino-y-del-adolescente/gdoc/">
-                      <div className="hover-text">
-                        <img src="img/logoGPA.png" alt="logo"></img>
-                      </div>{" "}
-                      <img
-                        src="img/portfolio/06-small.jpg"
-                        className="img-responsive img-gallery"
-                        alt="Project Title"
-                      />
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a href="https://venezuela.justia.com/federales/codigos/codigo-organico-procesal-penal/gdoc/">
-                      <div className="hover-text">
-                        <img src="img/logoGPA.png" alt="logo"></img>
-                      </div>{" "}
-                      <img
-                        src="img/portfolio/copp.jpg"
-                        className="img-responsive img-gallery"
-                        alt="Project Title"
-                      />
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a href="https://venezuela.justia.com/federales/constitucion-de-la-republica-bolivariana-de-venezuela/">
-                      <div className="hover-text">
-                        <img src="img/logoGPA.png" alt="logo"></img>
-                      </div>{" "}
-                      <img
-                        src="img/portfolio/constitucion.jpg"
-                        className="img-responsive img-gallery"
-                        alt="Project Title"
-                      />
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a href="https://venezuela.justia.com/federales/codigos/reforma-del-codigo-penal/gdoc/">
-                      <div className="hover-text">
-                        <img src="img/logoGPA.png" alt="logo"></img>
-                      </div>{" "}
-                      <img
-                        src="img/portfolio/codigopenal.jpg"
                         className="img-responsive img-gallery"
                         alt="Project Title"
                       />
